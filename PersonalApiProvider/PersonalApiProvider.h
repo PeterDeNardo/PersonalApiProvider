3c33b118c75710c86ac8a4d28a20196052465729
PeterDeNardo <peterdnardo@gmail.com>
2021-10-04 16:05:01 -0300

    Initial Commit

---
+++ PersonalApiProvider/PersonalApiProvider.h
@@ -0,0 +1,18 @@
+//
+//  PersonalApiProvider.h
+//  PersonalApiProvider
+//
+//  Created by Peter De Nardo on 04/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PersonalApiProvider.
+FOUNDATION_EXPORT double PersonalApiProviderVersionNumber;
+
+//! Project version string for PersonalApiProvider.
+FOUNDATION_EXPORT const unsigned char PersonalApiProviderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PersonalApiProvider/PublicHeader.h>
+
+
